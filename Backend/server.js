c9e8b776ad1fb43d9dde1417339fd0af1d1061a4
Sahil Baroha <sahilbaroha@gmail.com>
2026-02-10 10:59:09 +0530

    Fixed ECONNREFUSED for newer Node version

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,34 @@
+import dns from 'node:dns';
+
+// Force usage of Google DNS for this Node process
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
-
-
 dotenv.config();
+const Murl = process.env.MONGO_URL;
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost:27017/linkup");
 
-app.listen(3000, () => console.log("Server started on port 3000"));
+const start = async () => {
+    try {
+        await mongoose.connect(Murl, {
+            family: 4 // Force IPv4 (common fix for newer Node versions)
+        });
+        console.log("DB Connected Successfully!");
+    } catch (err) {
+        console.error("Critical Mongo Error:", err.message);
+        // This will tell us if it's still a DNS issue or something else like Auth
+    }
+}
+
+start();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
