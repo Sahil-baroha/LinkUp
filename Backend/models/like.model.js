a95822b75a1d3b4c9f37e4ca1a81ee781ddc4f96
Sahil Baroha <sahilbaroha@gmail.com>
2026-03-22 16:12:41 +0530

    feat(like): add like model schema

--- Backend/models/like.model.js
+++ Backend/models/like.model.js
@@ -1,23 +1,19 @@
 import mongoose from "mongoose";
 
-const likeSchema = mongoose.Schema({
+const likeSchema = new mongoose.Schema({
     postId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Post",
-        required: true,
-        index: true
+        required: true
     },
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
-        required: true,
-        index: true
+        required: true
     }
 }, { timestamps: true });
 
-// Compound unique index: one like per user per post
 likeSchema.index({ postId: 1, userId: 1 }, { unique: true });
 
 const Like = mongoose.model("Like", likeSchema);
-
 export default Like;
